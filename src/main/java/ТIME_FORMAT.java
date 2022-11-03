4a23544c1e0a936300d31680d77ce5efe807392b
Ilya Chernenko <93983025+ilacernenko12@users.noreply.github.com>
2022-11-03 17:07:21 +0300

    Update ТIME_FORMAT.java

--- src/main/java/ТIME_FORMAT.java
+++ src/main/java/ТIME_FORMAT.java
@@ -1,11 +1,11 @@
 import lombok.Getter;
 @Getter
-public class ÒIME_FORMAT {
-    int hh;
-    int mm;
-    int ss;
+public class Ã’IME_FORMAT {
+    private int hh;
+    private int mm;
+    private int ss;
 
-    public ÒIME_FORMAT(int hh, int mm, int ss) {
+    public Ã’IME_FORMAT(int hh, int mm, int ss) {
         this.hh = hh;
         this.mm = mm;
         this.ss = ss;
